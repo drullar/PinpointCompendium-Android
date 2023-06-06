68663a316620ab3a7a9a381475b93e84fb26d022
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2023-06-07 00:24:15 +0300

    Init android project

---
+++ app/src/main/java/com/example/pinpointcompendium_android/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pinpointcompendium_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
+
+
